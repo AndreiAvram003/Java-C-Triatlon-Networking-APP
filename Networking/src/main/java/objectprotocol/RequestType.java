261ab0fbfab214bb5dee1bd02b68354bab5ee4ea
Avram Andrei-Grigore <132280624+AndreiAvram003@users.noreply.github.com>
2024-07-17 18:43:16 +0300

    Delete Networking directory

--- Networking/src/main/java/objectprotocol/RequestType.java
+++
@@ -1,5 +0,0 @@
-package objectprotocol;
-
-public enum RequestType {
-    LOGIN,LOGOUT,GET_PARTICIPANTS,ADD_RESULT,FILTERED_PARTICIPANTS,POINTS_AT_TRIAL
-}
--- Networking/src/main/java/objectprotocol/ResponseType.java
+++
@@ -1,5 +0,0 @@
-package objectprotocol;
-
-public enum ResponseType {
-    OK,ERROR,PARTICIPANTS,FILTERED_PARTICIPANTS,RESULT_ADDED,POINTS_AT_TRIAL
-}
